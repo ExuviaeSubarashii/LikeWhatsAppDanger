dcc83a56c5769bebaa0775bd2cd02435a794a0d2
Ali Küçükkaya <preoyuncu@hotmail.com>
2024-02-15 12:02:38 +0300

    added name change, made styling changes

--- my-whats-app/src/Components/MainComponent.svelte
+++ my-whats-app/src/Components/MainComponent.svelte
@@ -8,11 +8,164 @@
 	let isModalVisible: boolean = false;
 	let message: string;
 	let messageType: string;
+	let receiverName=writable("");
+	let receiverInput:string;
 	function handleVisibility(messageTypeParam: string) {
 		isModalVisible = !isModalVisible;
 		messageType = messageTypeParam;
 	}
-	const chatMessage = writable<Messages[]>([]);
+	function handleReceiverNameChange(receiverInput:string){
+		if(receiverInput===''||receiverInput===undefined||receiverInput===null){
+            return alert('Lütfen bir isim giriniz.');
+        } 
+		receiverName.set(receiverInput);
+	}
+	const chatMessage = writable<Messages[]>([{
+			messageType: 'Gönderen',
+			messageContent: 'cxvx',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'werew',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'rewrwe',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'qweasd',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Gönderen',
+			messageContent: 'xvxc',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Gönderen',
+			messageContent: 'DFLK;GJDIOGJIOFDGJIODFJGJIODGJIDFJGJHDGJHEU4HGEUIRGHIDU',
+			timeStamp: '18:17'
+		},{
+			messageType: 'Gönderen',
+			messageContent: 'cxvx',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'werew',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'rewrwe',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'qweasd',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Gönderen',
+			messageContent: 'xvxc',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Gönderen',
+			messageContent: 'DFLK;GJDIOGJIOFDGJIODFJGJIODGJIDFJGJHDGJHEU4HGEUIRGHIDU',
+			timeStamp: '18:17'
+		},{
+			messageType: 'Gönderen',
+			messageContent: 'cxvx',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'werew',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'rewrwe',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'qweasd',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Gönderen',
+			messageContent: 'xvxc',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Gönderen',
+			messageContent: 'DFLK;GJDIOGJIOFDGJIODFJGJIODGJIDFJGJHDGJHEU4HGEUIRGHIDU',
+			timeStamp: '18:17'
+		},{
+			messageType: 'Gönderen',
+			messageContent: 'cxvx',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'werew',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'rewrwe',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'qweasd',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Gönderen',
+			messageContent: 'xvxc',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Gönderen',
+			messageContent: 'DFLK;GJDIOGJIOFDGJIODFJGJIODGJIDFJGJHDGJHEU4HGEUIRGHIDU',
+			timeStamp: '18:17'
+		},{
+			messageType: 'Gönderen',
+			messageContent: 'cxvx',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'werew',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'rewrwe',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Alıcı',
+			messageContent: 'qweasd',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Gönderen',
+			messageContent: 'xvxc',
+			timeStamp: '18:17'
+		},
+		{
+			messageType: 'Gönderen',
+			messageContent: 'DFLK;GJDIOGJIOFDGJIODFJGJIODGJIDFJGJHDGJHEU4HGEUIRGHIDU',
+			timeStamp: '18:17'
+		}]);
 
 	function HandleMessageCreation(content: string): any {
         if(content===''||content===undefined||content===null){
@@ -33,6 +186,10 @@
 	<div>
 		<button on:click={() => handleVisibility('Gönderen')}>Create Receiver Message</button>
 		<button on:click={() => handleVisibility('Alıcı')}>Create Sender Message</button>
+		<div>
+			<input placeholder="Alici Adi" bind:value={receiverInput}/>
+			<button on:click={()=>handleReceiverNameChange(receiverInput)}>Alici Adini Kaydet</button>
+		</div>
 	</div>
 </div>
 <PopupModal bind:showModal={isModalVisible} {messageType}>
@@ -60,18 +217,24 @@
 				<img src="https://www.w3schools.com/w3images/avatar2.png" alt="Avatar" />
 			</div>
 			<div class="name">
-				<span>Ali Kucukkaya</span>
+				{#key $receiverName}
+				<span>{$receiverName}</span>
+				{/key}
 			</div>
-			<div class="video">
-				<i class="bx bxs-video"></i>
-			</div>
-			<div class="call">
-				<i class="bx bxs-phone"></i>
-			</div>
-			<div class="more">
-				<i class="bx bx-dots-vertical-rounded"></i>
+			<div class="options">
+				<div class="video">
+					<i class="bx bxs-video"></i>
+				</div>
+				<div class="call">
+					<i class="bx bxs-phone"></i>
+				</div>
+				<div class="more">
+					<i class="bx bx-dots-vertical-rounded"></i>
+				</div>
 			</div>
 		</div>
+		<div class="messages">
+
 		{#key $chatMessage}
 			{#each $chatMessage as message}
 				<div class="message">
@@ -91,7 +254,7 @@
 							<div class="message-container">
 								<p class="message-content">{message.messageContent}</p>
 								<span class="timestamp-wrapper">
-									<span class="timestamp">{message.timeStamp}</span>
+									<span class="timestamp" style="color:lightgray">{message.timeStamp}</span>
 								</span>
 							</div>
 						</div>
@@ -100,19 +263,36 @@
 			{/each}
 		{/key}
 	</div>
+
+	</div>
 </div>
 
 <style>
+	.messages {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    max-height: 500px; /* Set the max height to whatever height you prefer */
+    overflow-y: scroll; /* Make it scrollable */
+}
+	.options{
+		position: relative;
+		left: 9%;
+		display: flex;
+		flex-direction: row;
+		top: 10px;
+	}
 	.call,
 	.video,
 	.more {
 		float: right;
-		margin: 0 0 0 10px;
 	}
 	.chat-container {
 		position: absolute;
 		top: 10%;
-		width: 90%;
+		width: 58%;
+		max-height: 10%;
+		
 	}
 	.avatar img {
 		border-radius: 50%;
@@ -126,14 +306,14 @@
 		position: relative;
 		top: 50%;
 	}
-	.more {
-		margin: 0 12px 0 32px;
-	}
 	.name {
 		font-size: 17px;
 		font-weight: 600;
 		letter-spacing: 0.3px;
 		white-space: nowrap;
+		top: 10px;
+		left: 10px;
+		position: relative;
 	}
 	.avatar {
 		margin: 0 0 0 5px;
@@ -150,28 +330,34 @@
 		font-size: 24px;
 		position: relative;
 		z-index: 1;
+		width: 27%;
 	}
 	.divright {
 		position: relative;
-		left: 20%;
-		max-width: 7%; /* Adjusted width for the message bubble */
+		left: 11%;
+		max-width: 15%;
 		padding: 10px;
 		border-radius: 20px;
 		margin-bottom: 10px;
 		word-wrap: break-word;
 		background: #e1ffc7;
 		color: black;
-		border-radius: 5px 0px 5px 5px;
+		top: 10px;
+		border-radius: 15px 0px 15px 15px;
 	}
 	.divleft {
-		background-color: white;
-		color: black;
+		background-color: rgb(31, 44, 52);
+		color: white;
 		position: relative;
-		max-width: 7%;
+		max-width: 15%;
 		padding: 10px;
 		border-radius: 20px;
 		margin-bottom: 10px;
 		word-wrap: break-word;
+		top: 10px;
+		border-radius: 0px 15px 15px 15px;
+
+
 	}
 	.message-container {
 		position: relative;
@@ -193,18 +379,17 @@
 		bottom: 4px;
 	}
 	.chat-body {
-		height: calc(100% - 68px);
+		display: flex;
+		flex-direction: column;
+		position: relative;
+		max-height: 10%;
 		box-shadow: inset 0 10px 10px -10px #000000;
-		overflow-x: hidden;
+		overflow-y: hidden;
 		padding: 0 16px;
 		margin-bottom: 5px;
-		height: calc(100% - 12px);
+		/* height: calc(100% - 12px); */
 		position: relative;
-		background: #efe7dd
-			url('https://cloud.githubusercontent.com/assets/398893/15136779/4e765036-1639-11e6-9201-67e728e86f39.jpg')
-			repeat;
-		z-index: 0;
-		height: 100%;
+	
 	}
 	.timestamp-wrapper .read {
 		display: inline-block;
